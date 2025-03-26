2e199ab83d9412149441efe428d1bb258e6e98e8
Douglas Silva <sdouglas204@outlook.com>
2025-03-25 22:58:00 -0300

    Create view

--- src/main/java/com/cursoJavaWeb/todosimple/configs/WebConfig.java
+++ src/main/java/com/cursoJavaWeb/todosimple/configs/WebConfig.java
@@ -9,7 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     
-    public void addCorsMappins(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
+    @Override  
+    public void addCorsMappings(CorsRegistry registry) {  
+        registry.addMapping("/**") // Permite todas as rotas  
+                .allowedOrigins("http://127.0.0.1:5500") // Permite acessos apenas desta origem  
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos  
+                .allowedHeaders("*") // Permite todos os cabeçalhos  
+                .allowCredentials(true); // Permite envio de cookies  
+    }  
 }
