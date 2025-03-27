2aa6be0c321438d2735cb3fc3eeec8436ac24453
Douglas Silva <sdouglas204@outlook.com>
2025-03-27 10:44:50 -0300

    Create JWTAuthorizationFilter

--- src/main/java/com/cursoJavaWeb/todosimple/configs/SecurityConfig.java
+++ src/main/java/com/cursoJavaWeb/todosimple/configs/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.cursoJavaWeb.todosimple.security.JWTAuthenticationFilter;
+import com.cursoJavaWeb.todosimple.security.JWTAuthorizationFilter;
 //import com.cursoJavaWeb.todosimple.security.JWTAuthorizationFilter;
 import com.cursoJavaWeb.todosimple.security.JWTUtil;
 
@@ -65,7 +66,7 @@ public class SecurityConfig {
                             .authenticationManager(authenticationManager);
 
             http.addFilter(new JWTAuthenticationFilter(this.authenticationManager, this.jwtUtil));
-            //http.addFilter(new JWTAuthorizationFilter(this.authenticationManager, this.jwtUtil, this.userDetailsService));
+            http.addFilter(new JWTAuthorizationFilter(this.authenticationManager, this.jwtUtil, this.userDetailsService));
 
             http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
